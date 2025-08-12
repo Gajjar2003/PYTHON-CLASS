3162fd084606d455a196d775ceb1b1d0578b8690
JENIL1108 <jenilgajjar1108@gmail.com>
2025-08-12 14:55:28 +0530

    data

---
+++ Class_work/Data_Type.py
@@ -0,0 +1,45 @@
+# Numeric Data Types
+# int ,flot,complex
+
+a=10
+b=115.8
+c=5j+6
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+#----------------------------------------------------------
+
+
+# Sequence Data Types
+#  str,list,tuple,range
+
+name="jenil"
+student=["jenil","yug","meet","abhi","ravi"]
+t=("j","e","n","i","l",1108)
+
+print(type(name))
+print(type(student))
+print(type(t))
+
+
+# --------------------------------------------------------
+
+# Mapping , Set ,Boolean Data Types
+
+s={"tops","tech","surat"}
+m={"jenil": "Gajjar", "age" : 22}
+bool = True
+
+print(type(s))
+print(type(m))
+print(type(bool))
+
+# -----------------------------------------------  
+
+# User to input 
+
+a = input("Please enter your number is:")
+print("your number is =" ,a)
--- Class_work/hello.py
+++ Class_work/hello.py
@@ -1,1 +1,13 @@
 print("well come to python course ")
+
+print('hello ", python')
+print("Hello ' , python")
+print(""" Well come
+      to python
+      lanuage
+      starting for august  """)
+
+print("Hello","python","Hello","jenil","Hello","java", sep="||")
+print("Well \' come to ")
+print('Well \" come to ')
+print("Well "and "come to python")
