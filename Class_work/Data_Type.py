bb14d505ec30c846e90f6846d712cc1c99ef831e
JENIL1108 <jenilgajjar1108@gmail.com>
2025-08-18 17:08:18 +0530

    ch

--- Class_work/Data_Type.py
+++ Class_work/Data_Type.py
@@ -1,13 +1,13 @@
 # Numeric Data Types
 # int ,flot,complex
 
-a=10
-b=115.8
-c=5j+6
+# a=10
+# b=115.8
+# c=5j+6
 
-print(type(a))
-print(type(b))
-print(type(c))
+# print(type(a))
+# print(type(b))
+# print(type(c))
 
 
 #----------------------------------------------------------
@@ -16,30 +16,30 @@ print(type(c))
 # Sequence Data Types
 #  str,list,tuple,range
 
-name="jenil"
-student=["jenil","yug","meet","abhi","ravi"]
-t=("j","e","n","i","l",1108)
+# name="jenil"
+# student=["jenil","yug","meet","abhi","ravi"]
+# t=("j","e","n","i","l",1108)
 
-print(type(name))
-print(type(student))
-print(type(t))
+# print(type(name))
+# print(type(student))
+# print(type(t))
 
 
 # --------------------------------------------------------
 
 # Mapping , Set ,Boolean Data Types
 
-s={"tops","tech","surat"}
-m={"jenil": "Gajjar", "age" : 22}
-bool = True
+# s={"tops","tech","surat"}
+# m={"jenil": "Gajjar", "age" : 22}
+# bool = True
 
-print(type(s))
-print(type(m))
-print(type(bool))
+# print(type(s))
+# print(type(m))
+# print(type(bool))
 
 # -----------------------------------------------  
 
 # User to input 
 
-a = input("Please enter your number is:")
-print("your number is =" ,a)
+# a = input("Please enter your number is:")
+# print("your number is =" ,a)
--- Class_work/hello.py
+++ Class_work/hello.py
@@ -1,13 +1,13 @@
-print("well come to python course ")
+# print("well come to python course ")
 
-print('hello ", python')
-print("Hello ' , python")
-print(""" Well come
-      to python
-      lanuage
-      starting for august  """)
+# print('hello ", python')
+# print("Hello ' , python")
+# print(""" Well come
+#       to python
+#       lanuage
+#       starting for august  """)
 
-print("Hello","python","Hello","jenil","Hello","java", sep="||")
-print("Well \' come to ")
-print('Well \" come to ')
-print("Well "and "come to python")
+# print("Hello","python","Hello","jenil","Hello","java", sep="||")
+# print("Well \' come to ")
+# print('Well \" come to ')
+# print("Well "and "come to python")
