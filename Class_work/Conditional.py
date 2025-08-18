f219e778d8f7910fc4933f30bc4a6f6b7502af7e
JENIL1108 <jenilgajjar1108@gmail.com>
2025-08-18 17:04:56 +0530

    if else

---
+++ Class_work/Conditional.py
@@ -0,0 +1,49 @@
+#conditional statement
+
+#if-else
+
+# age = 20
+
+# if age > 18:
+#     print("You are young ")
+# else:
+#     print("You are child")
+
+
+# if-else and if-else
+
+# a =100
+# b = 150
+# c = 110
+
+# if a > b:
+#     print("A is greater")
+# else:
+#     print("B is greater")
+
+# if a>c :
+#         print("A is greater")
+# else:
+#         print("C is greater")
+
+# if b>c :        
+#         print("B is greater")
+# else :
+#         print("C is greater")        
+
+
+
+# if-else ladder
+
+# a = 100
+# b = 200
+# c = 300
+
+# if a > b and a > c:
+#     print("A is greater")
+# elif b > a and b > c:
+#     print("B is greater")
+# else:
+#     print("C is greater")        
+
+
---
+++ Class_work/Extra-Example.py
@@ -0,0 +1,18 @@
+# extra example foe if-else statement
+# student marks sheet program
+
+# print("------Student marks program--------")
+
+# marks = int(input("Please enter your marks is: "))
+
+# if marks > 90:
+#     print("You are A grade")
+# elif marks < 90 and marks > 85:
+#     print("You are B grade")    
+# elif marks < 80 and  marks > 70:
+#     print("You are C garde")
+# elif marks < 70 and marks > 50:
+#     print("You are D grade")
+# else:
+#     print("You are fail it !")
+
