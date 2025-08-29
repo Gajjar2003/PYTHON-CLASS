13c3cc90a5419809e2bf35fc74b664ca6039ed8b
JENIL1108 <jenilgajjar1108@gmail.com>
2025-08-29 14:13:21 +0530

    string

---
+++ Class_work/string prgram/string.py
@@ -0,0 +1,19 @@
+#String Length
+
+# string = "well come to be python course"
+# print(len(string))
+
+#----------------------------------------------------------
+
+# String Upper Case
+
+# string = "jenil gajjar"
+# print(string.upper())
+
+
+#---------------------------------------------------------
+
+# String Lower Case
+
+# string = "JENIL GAJJAR"
+# print(string.lower())
