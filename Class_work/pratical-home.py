546a01cfeeb846688066f5d4de17d45c7b75da3b
JENIL1108 <jenilgajjar1108@gmail.com>
2025-08-29 14:00:17 +0530

    all

--- Class_work/pratical-home.py
+++ Class_work/pratical-home.py
@@ -106,33 +106,6 @@
 # else:
 #     print(temp, "is Not a Palindrome number")
 
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # -----------------------------------------------------------------------------
 
 
@@ -188,9 +161,9 @@
 
 
 # for i in range(1, 6):       
-#     for j in range(5, i, -1):   
+#     for k in range(5, i, -1):   
 #         print(" ", end=" ")
-#     for k in range(1, i+1):     
+#     for j in range(1, i+1):     
 #         print("*", end=" ")
 #     print()   
  
@@ -217,7 +190,54 @@
 #     print()  
 
      
+#-------------------------------------------------------------------------------------
+
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*", end="")
+#     print()    
+
+# for i in range(6,0,-1):
+#     for j in range(i):
+#         print("*",end="")
+#     print()    
+
+# for i in range(1,5):
+#     for j in range(1,5):
+#         print(" * ",end="")
+#     print()    
+
+
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print(" * ",end="")
+#     print()    
+
+
+# for i in range(1,6):
+#     for k  in range(5,i,-1):
+#         print(" ",end="")
+#     for j in range(1,i+1):
+#         print("*",end="")
+#     print()
+
+
+# for i in range(5):
+#     for k in range(i):
+#         print(" ", end="")
+#     for j  in range(4):
+#          print(" * ",end="")
+#     print()        
 
 
 
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*",end="")
+#     print() 
 
+# for i in range(5 ,0 ,-1):
+#     for j in range(i):
+#         print("*",end="")
+#     print()    
