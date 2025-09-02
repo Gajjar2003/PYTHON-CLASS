437ba167d60fed2214c86cc28ab86707f3eb4915
JENIL1108 <jenilgajjar1108@gmail.com>
2025-09-02 16:00:33 +0530

    ass

---
+++ Python_Assignment/Partical-python/p1.py
@@ -0,0 +1,4 @@
+#  Write a Python program that prints "Hello, World!".
+
+print("Hellp World !")
+print("My name is jenil gajjar !")
---
+++ Python_Assignment/Partical-python/p2.py
@@ -0,0 +1,12 @@
+# Write a Python program that demonstrates the correct use of indentation, comments, and
+# variablesfollowing PEP 8 guidelines.
+
+
+#   My name is jenil gajjar
+#   well come to be python coures,
+#   i am indentation, comments and,
+#   varivables PEP guideline
+
+
+# num = 12
+# print(num)
