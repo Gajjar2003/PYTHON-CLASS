e3ea79a22ba8a73c2cea48310e784de8e47749b0
JENIL1108 <jenilgajjar1108@gmail.com>
2025-09-03 20:32:13 +0530

    all

--- Class_work/string prgram/string.py
+++ Class_work/string prgram/string.py
@@ -21,4 +21,122 @@
 
 #--------------------------------------------------------
 
-# String 
+# String capitalize
+
+# string = "jenil gajjar"
+# print(string.capitalize())
+
+
+# ---------------------------------------------------------
+
+# String title 
+
+# string = "jenil gajjar"
+# print(string.title())
+
+# ---------------------------------------------------------------
+
+# String strip 
+
+# string = " jenil gajjar "
+# print(string.strip())
+
+# --------------------------------------------------------------
+
+# String replace
+
+# string = "jenil gajjar"
+# print (string.replace("j","a",2))
+
+
+# --------------------------------------------------------------
+
+# String find
+
+# string = "jenil gajjar"
+# print(string.find("jenil"))
+
+# --------------------------------------------------------------
+
+# String startswith
+
+# string = "jenil "
+# print (string.startswith("j"))
+
+#--------------------------------------------------------------
+
+# String endswith
+
+# string = "jenil"
+# print(string.endswith("l"))
+
+# --------------------------------------------------------------
+
+# String spilt
+
+# string = "jenil gajjer"
+# print(string.split(" ",3))
+
+#--------------------------------------------------------------
+
+# String join
+
+
+# print("jenil".join("gajjar"))
+
+#-------------------------------------------------------------
+
+# String isalpha
+
+# string = "jenil"
+# print(string.isalpha())
+
+#--------------------------------------------------------------
+
+# String isdigit
+
+# string = "123456789"
+# print(string.isdigit())
+
+#-------------------------------------------------------------
+
+# String isalnum
+
+# string = "Jenilgajjar1108"
+# print(string.isalnum())
+
+# ----------------------------------------------------------
+
+# String zfill
+
+# string = "jenil gajjar"
+# print(string.zfill(20))
+
+#-----------------------------------------------------------
+
+# String center
+
+# string = "jenil gajjar"
+# print(string. center(18 ,"*"))
+
+#-------------------------------------------------------------
+
+
+
+# name = "hello python hello 4545 ddds 545454"
+
+# a = 0
+# b = 0
+
+# for i in name:
+#     if str(i).isalpha():
+#         a += 1
+#     elif str(i).isdigit():
+#         b += 1
+
+# print ("alpha is ", a)
+# print ( "digit is ", b)
+
+
+
+# ---------------------------------------------------------------
