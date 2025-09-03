3f32bbc28a5161af5a53b1003521ee4036b060df
JENIL1108 <jenilgajjar1108@gmail.com>
2025-09-03 10:06:59 +0530

    ass

---
+++ Python_Assignment/Partical-python/p3.py
@@ -0,0 +1,33 @@
+# Write a Python program to demonstrate the creation of variables and different data types.
+
+Age = 22
+
+print("Age: " , Age," Type: ",type(Age))
+
+Price = 180.54
+
+print("Price: ", Price, " Type :", type(Price))
+
+Name = "Jenil"
+
+print("Name : ",Name , "Type : ",type(Name))
+
+Boll = True
+
+print("Boll : ", Boll , "Type : ", type(Boll))
+
+
+Fruits =  ["apple", "banana", "cherry"]
+
+print ("Fruits : ", Fruits , "Type : ",type(Fruits))
+
+Code = (10.5,45.8)
+
+print("Code : ", Code , "Type : ", type(Code))
+
+Number = {1,2,3,4,5,6,7,8,9}
+
+print("Number : ", Number , "Type : ", type(Number))
+
+Student = {"name":"jenil","age": 22,"coures": "python"}
+
