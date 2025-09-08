f886d3aec2314cf3f60526ea25de7c838313d100
JENIL1108 <jenilgajjar1108@gmail.com>
2025-09-08 15:40:59 +0530

    assess

---
+++ Python_Assessment/Assessment.py
@@ -0,0 +1,48 @@
+# Create a mini-project where students combine conditional statements, loops, and functions
+# to create a basic Python application, such as a simple calculator or a grade management
+# # system.
+
+print("****************************************************")
+print("*                                                  *")
+print("*           Grade Management System                *")
+print("*                                                  *")
+print("****************************************************")
+
+choice = "y"
+
+
+
+while choice.lower() == "y":
+    
+    name = input("Enter your student name: ")
+    marks = int(input("Enter your marks (0-100): "))
+
+
+
+  
+    if marks >= 90:
+        grade = "A+ Grade"
+    elif marks >= 80:
+        grade = "A Grade"
+    elif marks >= 70:
+        grade = "B Grade"
+    elif marks >= 60:
+        grade = "C Grade"
+    elif marks >= 50:
+        grade = "D Grade"
+    elif marks >= 40:
+        grade = "E Grade"
+    else:
+        grade = "Fail!"
+
+
+    
+    choice = input("Do you want to continue? (y/n): ")
+print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+print("Student name is",name)
+print("student marks is",marks)
+print("Student grade",grade)
+
+
+
+
