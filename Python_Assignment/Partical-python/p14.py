c93d664a7b73617c96a422cf3ef7185bc2bb4761
JENIL1108 <jenilgajjar1108@gmail.com>
2025-09-08 13:33:02 +0530

    ass

---
+++ Python_Assignment/Partical-python/p14.py
@@ -0,0 +1,33 @@
+# Write a Python program to demonstrate string slicing
+
+input = "hello world python !"
+
+print(input[0:9])
+print(input[4:])
+print(input[:5])
+print(input[1:10])
+print(input[::5])
+print(input[::-1])
+
+
+print("**************************************************************")
+
+# Write a Python program that manipulates and prints strings using various string methods.
+
+text = "hello python world !"
+
+print(text.upper())
+print(text.lower())
+print(text.capitalize())
+print(text.title())
+print(text.strip())
+print(text.startswith("hello"))
+print(text.endswith("!"))
+print(text.find("python"))
+print(text.count("l"))
+print(text.replace("l","a",1))
+print(text.split())
+
+
+
+
