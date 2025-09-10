ace4ce5e773c62dc6bd862f7768622c90440f3bc
JENIL1108 <jenilgajjar1108@gmail.com>
2025-09-10 13:48:45 +0530

    example

--- Class_work/pratical-home.py
+++ Class_work/pratical-home.py
@@ -241,3 +241,5 @@
 #     for j in range(i):
 #         print("*",end="")
 #     print()    
+
+
---
+++ Class_work/python-examp[le.py
@@ -0,0 +1,85 @@
+# Write a Python program to take a user’s name and age as input and print them.
+
+# name = input("Enter your name is:- ")
+# age =int(input("Enter you age is:- "))
+
+# print("your name is",name)
+# print("your age is",age)
+# print(type(name))
+# print(type(age))
+
+# Write a Python program that takes two numbers as input and prints their sum.
+
+# n1 =int(input("enter first number: "))
+# n2 = int(input("enter second number: "))
+# sum = n1 + n2
+# print(n1, "and",n2 ,"sum is ", sum)
+
+# Write a program to swap two numbers without using a third variable.
+
+# n1 =int(input("enter first number: "))
+# n2 = int(input("enter second number: "))
+
+# n1 = n1 + n2
+# n2 = n1 - n2
+# n1 = n1 - n2
+
+# print("enter your number is swaaping number n1 = ",n1 ,"and n2 = ",n2)
+
+
+# Write a program to find the remainder when one number is divided by another.
+
+# n1 =int(input("enter first number: "))
+# n2 = int(input("enter second number: "))
+
+# rem = n1 % n2
+# print(n1,"and ",n2 , "rem is",rem)
+
+
+
+# Write a Python program to check if a number is even or odd.
+
+# num =int(input("enter first number: "))
+
+
+# if num %2 == 0:
+#     print("Enter your number is even")
+# else:
+#     print("enter your number is odd")    
+
+
+# Write a program to check if a given year is a leap year.
+
+# num =int(input("enter your years: "))
+
+# if num %4 == 0:
+#     print("Enter your years leep years")
+# else:
+#     print("Enter your years is not leep years")    
+
+
+
+# Write a program to find the greatest of three numbers.
+
+
+# n1 =int(input("enter first number: "))
+# n2 = int(input("enter second number: "))
+# n3 = int(input("Enter three number:- "))
+
+# if n1 > n2 and n1 > n3:
+#     print("first number greatest ",n1)
+# elif n2 > n1 and n2 > n3:
+#     print("second number greatest",n2)
+# else:
+#     print("three number greatest",n3) 
+
+
+# 🔹 Loops
+
+# Write a program to print numbers from 1 to 10 using a loop.
+
+# Write a program to print a multiplication table of a given number.
+
+# Write a program to calculate the factorial of a number using a loop.
+
+# Write a program to find the sum of digits of a number.
