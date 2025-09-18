9e9ccb8686e55f77b2a31029d5eab85e192dfcef
JENIL1108 <jenilgajjar1108@gmail.com>
2025-09-18 17:23:50 +0530

    all

---
+++ Python_Assignment/Practical-pyhton-7/2. List Operations.py
@@ -0,0 +1,44 @@
+#  Write a Python program to add elements to a list using insert() and append().
+
+list = [1,2,3,4]
+list.append(5)
+print(list)
+
+
+list = [1,2,3,4]
+list.insert(4,5)
+print(list)
+
+print("*******************************************************************")
+
+# Write a Python program to remove elements from a list using pop() and remove().
+
+list = [1,2,3,4]
+list.remove(4)
+print(list)
+
+list = [1,2,3,4,5]
+list.pop()
+print(list)
+
+print("*******************************************************************")
+
+# Write a Python program to update a list using insert() and append()
+
+student = ["jenil","gajjar"]
+student.append("ramchandra")
+print(student)
+
+std = ["age","is"]
+std.insert(2,21)
+print(std)
+
+# Write a Python program to remove elements from a list using pop() and remove().
+
+student = ["jenil","gajjar"]
+student.remove("gajjar")
+print(student)
+
+std = ["age","is"]
+std.pop()
+print(std)
---
+++ Python_Assignment/Practical-pyhton-7/4. Tuple.py
@@ -0,0 +1,55 @@
+# Write a Python program to create a tuple with multiple data types.
+
+tuple = (1,10,21.4,51,"jenil","gajjar",True)
+
+print(tuple)
+print(type(tuple))
+
+print("***********************************************************************")
+
+# Write a Python program to concatenate two tuples.
+
+t1 = ("jenil","gajjar")
+t2 = ("age",21)
+
+t3 = t1 + t2
+print(t3)
+
+print("***********************************************************************")
+
+# Write a Python program to convert a list into a tuple.
+
+tuple = (1,10,21.4,51,"jenil","gajjar",True)
+
+l = list(tuple)
+print(l)
+print(type(l))
+
+
+print("***********************************************************************")
+
+# Write a Python program to create a tuple with multiple data
+
+
+tuple = (1,10,21.4,51,"jenil","gajjar",True)
+
+print(tuple)
+print(type(tuple))
+
+print("***********************************************************************")
+
+# Write a Python program to concatenate two tuples into one. 
+
+t1 = ("jenil")
+t2 = ("gajjar")
+
+t3 = t1+t2
+print(t3)
+
+print("***********************************************************************")
+
+# Write a Python program to access the value of the first index in a tuple
+
+tuple = (1,10,21.4,51,"jenil","gajjar",True)
+
+print(tuple[1])
