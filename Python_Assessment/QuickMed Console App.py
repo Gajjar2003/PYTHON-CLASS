8be63a54f3a2e2b1ba68f111db152901f68db909
JENIL1108 <jenilgajjar1108@gmail.com>
2025-09-24 17:33:46 +0530

    ass

---
+++ Python_Assessment/QuickMed Console App.py
@@ -0,0 +1,17 @@
+# Module 6) Python - Fundamentals of python language
+# Module 7) Python - Collections, functions and Modulesin Python
+# QuickMed Console App
+
+print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+print("*            Quickmed pharmacy              *")
+print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+
+print("Press 1 is Add medicine  ")
+print("Press 2 is View inventory  ")
+print("Press 3 is Update stock  ")
+print("Press 4 is Sell medicine  ")
+print("Press 5 is View sell report  ")
+print("Press 6 is Exit ")
+
+
+choice = input("Please enter your choice is : ")
