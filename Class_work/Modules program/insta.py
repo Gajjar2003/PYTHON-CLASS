57d6894d6aa8753e7fbd400ea9bdb8870a409d4f
JENIL1108 <jenilgajjar1108@gmail.com>
2025-09-24 17:09:53 +0530

    all

--- Class_work/Modules program/insta.py
+++ Class_work/Modules program/insta.py
@@ -5,3 +5,5 @@ loader = instaloader.Instaloader()
 name = input("Enter Instagram username: ")
 
 loader.download_profile(name, profile_pic_only=True)
+
+
--- Class_work/python-example-for-exam/function.py
+++ Class_work/python-example-for-exam/function.py
@@ -60,3 +60,80 @@ def largest(a,b,c):
         return c
     
 print(largest(120,45,755))
+
+
+print("************************************************************************")
+
+# Write a function to check if a string is a palindrome.
+
+def is_palindrome_number(number):
+    temp = number
+    rev = 0
+
+    while number != 0:
+        rem = number % 10
+        rev = rev * 10 + rem
+        number //= 10
+
+    if rev == temp:
+        print(temp, "is a Palindrome number")
+    else:
+        print(temp, "is Not a Palindrome number")
+
+
+is_palindrome_number(121)
+is_palindrome_number(123)
+
+
+print("************************************************************************")
+
+# Write a function to find the Fibonacci sequence up to n terms.
+
+def fibobaccci(a):
+    temp = number
+    pe = 0 
+    pr = 1
+
+    for i in range(a):
+        temp= pe+pr
+        pe = pr
+        pr =temp
+
+        print(temp,"fibonacci ")
+
+fibobaccci(10)
+
+
+
+print("************************************************************************")
+
+# Write a function that accepts a list of numbers and returns the maximum element.
+
+def find_max(nums):
+    return max(nums)
+
+
+print(find_max([12, 45, 2, 89, 34]))
+
+print("************************************************************************")
+
+# Write a function to calculate the area and perimeter of a rectangle.
+
+def rectangle_area_perimeter(length, width):
+    area = length * width
+    perimeter = 2 * (length + width)
+    return area, perimeter
+
+
+a, p = rectangle_area_perimeter(100, 5)
+print("Area:", a)
+print("Perimeter:", p)
+
+
+print("************************************************************************")
+
+# Write a function that takes a list and returns a new list with only unique elements.
+def unique(lst):
+    return list(set(lst))
+
+print(unique([1, 45, "%", "@", 45]))
