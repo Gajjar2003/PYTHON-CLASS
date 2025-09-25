253a9dd4e312eb0923ed5ce25d8405e3fb082ba3
JENIL1108 <jenilgajjar1108@gmail.com>
2025-09-25 17:21:27 +0530

    all

--- Class_work/File_handling_program/f1.py
+++ Class_work/File_handling_program/f1.py
@@ -16,3 +16,61 @@ print(data)
 f = open("test.txt",'a')
 f.write("\n Hello Java")
 f.close()
+
+
+# file handling readline mode 
+
+f = open("test.txt",'r')
+while True:
+    data = f.readline()
+    print(len(data))
+    if 'Java' in data:
+        print(data)
+    if not data:
+        break
+
+
+# file handling readlines mode 
+
+f = open("test.txt",'r')
+data = f.readlines()
+print(data)
+
+
+# file handling with mode without file close mode 
+
+
+with open("test.txt","r") as f:
+    print(f.tell())
+    f.seek(2)
+    print(f.tell())
+    data = f.read()
+    print(f.tell())
+    print(data)
+
+
+#  file handling read + mode 
+
+# with open('test1.txt','r+') as f:
+#     f.write("something")
+#     f.seek(0)
+#     data = f.read()
+#     print(data)
+
+
+
+with open('test1.txt','w+') as f:
+    f.write("something")
+    f.seek(0)
+    data = f.read()
+    print(data)
+
+
+
+#  file handling for image read for binary mode 
+
+# with open("e:\photos\jenil photos\DROB6864.JPG","rb") as f:
+#     data = f.read()
+#     print(data)
+
+
