8ff06116908c94979ad3319d89b56faf490a51c3
JENIL1108 <jenilgajjar1108@gmail.com>
2025-09-25 17:55:38 +0530

    ass

--- Python_Assessment/QuickMed Console App.py
+++ Python_Assessment/QuickMed Console App.py
@@ -1,17 +1,64 @@
 # Module 6) Python - Fundamentals of python language
 # Module 7) Python - Collections, functions and Modulesin Python
 # QuickMed Console App
+medicine_stock = {}
 
-print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
-print("*            Quickmed pharmacy              *")
-print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+def add_medicine():
+    print("*-*-*-*-*-*-*-*-*-* Add Medicine *-*-*-*-*-*-*-*-*-*-*-*-*")
 
-print("Press 1 is Add medicine  ")
-print("Press 2 is View inventory  ")
-print("Press 3 is Update stock  ")
-print("Press 4 is Sell medicine  ")
-print("Press 5 is View sell report  ")
-print("Press 6 is Exit ")
+    name = input("Enter medicine name: ").strip()
+    price = float(input("Enter medicine price: "))
+    stock = int(input("Enter medicine stock: "))
+
+  
+    medicine_stock[name] = [price, stock]
+
+    print(f"✅ Medicine '{name}' added successfully!")
+
+    add_choice = input("Do you want to add another medicine? (y/n): ").lower()
+    if add_choice == "y":
+        add_medicine()  
+    else:
+        print("Returning to main menu...")
 
 
-choice = input("Please enter your choice is : ")
+def view_inventory():
+    print("*-*-*-*-*-*-*-*-*-* View Inventory *-*-*-*-*-*-*-*-*-*-*-*-*")
+
+    if not medicine_stock:  
+        print("⚠️ Inventory is empty.")
+        return
+
+    for med, details in medicine_stock.items():
+        print(f"{med} - Price: ₹{details[0]}, Stock: {details[1]}")
+
+
+
+
+
+
+
+
+# while choice != "n":
+
+
+#     print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+#     print("*            Quickmed pharmacy              *")
+#     print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+
+#     print("Press 1 is Add medicine  ")
+#     print("Press 2 is View inventory  ")
+#     print("Press 3 is Update stock  ")
+#     print("Press 4 is Sell medicine  ")
+#     print("Press 5 is View sell report  ")
+#     print("Press 6 is Exit ")
+
+
+#     option = input("Please enter your choice is : ")
+
+
+
+
+#     choice = input("Do you want to continue(y/n) : ")
+
+
