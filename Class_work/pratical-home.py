fcd83559a1b04a65434941bcb2a12e53d1abc0f0
JENIL1108 <jenilgajjar1108@gmail.com>
2025-09-29 17:12:46 +0530

    all

--- Class_work/pratical-home.py
+++ Class_work/pratical-home.py
@@ -290,5 +290,44 @@ print("Name\tMarks\tGrade")
 for student in students:
     print(f"{student['name']}\t{student['marks']}\t{student['grade']}")
 
+# ----------------------------------------------------------------------------------------------------------------------------------------------
 
+print("****************************************************")
+print("*                                                  *")
+print("*         simple calculator system                 *")
+print("*                                                  *")
+print("****************************************************")
+
+choice  = "y"
+
+while choice != "n":
+
+    print("Select operation : ")
+    print("Press 1 is addition (+): ")
+    print("Press 2 is Subtraction  (-): ")
+    print("Press 3 is Multiplication  (*): ")
+    print("Press 4 is Division  (/): ")
+
+    option = input("Enter your choice is (1-4) : ")
+
+    num1 = int(input("Please enter your first  number : "))
+    num2 = int(input("Now secode number is : "))
+
+    if option == "1":
+        print(f"{num1} and {num2} addition is :  {num1+num2}")
+
+    elif option == "2":
+        print(f"{num1} and {num2} Subtraction is :  {num1-num2}")
+
+    elif option == "3":
+        print(f"{num1} and {num2} Multiplication is :  {num1*num2}")
+
+    elif option == "4":
+        print(f"{num1} and {num2} Division is :  {num1/num2}")
+        
+    else:
+        print("Invalid choice ! ")
+  
+    
+    choice = input("Do you want to continue? (y/n): ")
 
