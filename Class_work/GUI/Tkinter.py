39c8f22cacf6881b27743756592dbf423f5a3e52
JENIL1108 <jenilgajjar1108@gmail.com>
2025-11-04 17:18:42 +0530

    all

---
+++ Class_work/GUI/Tkinter.py
@@ -0,0 +1,47 @@
+# from tkinter import *
+
+# root = Tk()
+
+# ---------------------------------- PACK Tkinter---------------------------------------------
+
+# root.geometry("700x500")
+
+# Button(root, text="LEFT").pack(side=LEFT)
+# Button(root, text="RIGHT").pack(side=RIGHT)
+# Button(root, text="TOP").pack(side=TOP)
+# Button(root, text="BOTTOM").pack(side=BOTTOM)
+
+# root.mainloop()
+
+
+# ----------------------------------- GRID Tkinter -------------------------------------------------
+
+# root.geometry("700x500")
+
+# Label(root, text="Name").grid(row=1, column=1, padx=10, pady=10)
+# Label(root, text="Email").grid(row=2, column=1, padx=10, pady=10)
+# Label(root, text="Phone").grid(row=3, column=1, padx=10, pady=10)
+
+# Entry(root).grid(row=1, column=2, padx=10, pady=10)
+# Entry(root).grid(row=2, column=2, padx=10, pady=10)
+# Entry(root).grid(row=3, column=2, padx=10, pady=10)
+
+# Button(root, text="Submit").grid(row=4, column=2, pady=20)
+
+# root.mainloop()
+
+# ------------------------- PLACE Tkinter ------------------------------
+
+# root.geometry("700x500")
+
+# Label(root, text="Name").place(x=200, y=100)
+# Label(root, text="Email").place(x=200, y=150)
+# Label(root, text="Phone").place(x=200, y=200)
+
+# Entry(root).place(x=300, y=100)
+# Entry(root).place(x=300, y=150)
+# Entry(root).place(x=300, y=200)
+
+# Button(root, text="Submit", width=15).place(x=300, y=230)
+
+# root.mainloop()
