b4f86fd938bd92625967e9eb5f88308d201f2474
JENIL1108 <jenilgajjar1108@gmail.com>
2025-11-27 17:59:23 +0530

    all

--- Class_work/14-Django/myapp/urls.py
+++ Class_work/14-Django/myapp/urls.py
@@ -3,5 +3,8 @@ from myapp.views import *
 
 urlpatterns = [
 
-    path("", index,name="index")
+    path("", index,name="index"),
+    path("about",about,name="about"),
+     path("phone", phone,name="phone"),
+     path("help",help,name="help")
 ]
--- Class_work/14-Django/myapp/views.py
+++ Class_work/14-Django/myapp/views.py
@@ -5,4 +5,12 @@ from django.shortcuts import render
 
 def index(request):
         return render(request,"index.html")
+
+def about(request):
+        return render(request,"about.html")
    
+def phone(request):
+        return render(request,"phone.html")
+
+def help(requset):
+        return render(requset,"help.html")
