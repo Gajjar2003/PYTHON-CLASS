5bf27c557eddfe8bdb68f8633b4f6fe8b84df082
JENIL1108 <jenilgajjar1108@gmail.com>
2025-12-10 17:16:18 +0530

    all

--- Class_work/14-Django/Eshop-project/eshopapp/urls.py
+++ Class_work/14-Django/Eshop-project/eshopapp/urls.py
@@ -1,20 +1,19 @@
 from django.urls import path
 from eshopapp.views import * 
 
-
 urlpatterns = [
-
-    path("", index ,name="index"),
-    path("about", about,name="about"),
-    path("blog-details",blog_details, name="blog-details"),
-    path("blog", blog,name="blog"),
-    path("checkout",checkout,name="checkout"),
-    path("contact",contact,name="contact"),
-    path("main",main,name="main"),
-    path("shop-details",shop_details,name="shop-details"),
-    path("shop",shop,name="shop"),
-    path("shopping-cart",shopping_cart,name="shopping-cart")
-
-
-
+    path("", index, name="index"),
+    path("about", about, name="about"),
+    path("blog-details", blog_details, name="blog-details"),
+    path("blog", blog, name="blog"),
+    path("checkout", checkout, name="checkout"),
+    path("contact", contact, name="contact"),
+    path("main", main, name="main"),
+    path("shop-details", shop_details, name="shop-details"),
+    path("shop", shop, name="shop"),
+    path("shopping-cart", shopping_cart, name="shopping-cart"),
+    path("login",login,name="login"),
+    path("user-register",user_register,name="user-register"),
+    path("user-login",user_login,name="user-login")
+    
 ]
--- Class_work/14-Django/Eshop-project/eshopapp/views.py
+++ Class_work/14-Django/Eshop-project/eshopapp/views.py
@@ -1,31 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+def index(request):
+    return render(request,"index.html")
 
-def index(requset):
-    return render(requset,"index.html")
+def about(request):
+    return render(request,"about.html")
 
-def about(requset):
-    return render(requset,"about.html")
+def blog_details(request):
+    return render(request,"blog-details.html")
 
-def blog_details(requset):
-    return render(requset,"blog-details.html")
+def blog(request):
+    return render(request,"blog.html")
 
-def blog(requset):
-    return render(requset,"blog.html")
+def checkout(request):
+    return render(request,"checkout.html")
 
-def checkout(requset):
-    return render(requset,"checkout.html")
+def contact(request):
+    return render(request,"contact.html")
 
-def contact(requset):
-    return render(requset,"contact.html")
+def main(request):
+    return render(request,"main.html")
 
-def main(requset):
-    return render(requset,"main.html")
+def shop_details(request):
+    return render(request,"shop-details.html")
 
-def shop_details(requset):
-    return render(requset,"shop-details.html")
+def shop(request):
+    return render(request,"shop.html")
 
-def shop(requset):
-    return render(requset,"shop.html")
+def shopping_cart(request):
+    return render(request,"shopping-cart.html")
 
-def shopping_cart(requset):
-    return render(requset,"shopping-cart.html")
+def login(requset):
+    return render(requset,"login.html")
+
+def user_register(request):
+    if request.method == 'POST':
+        data = request.POST
+        name = data.get('name')
+        email = data.get('email')
+        password = data.get("pass")
+      
+        u = User(username=name, email=email)
+        u.set_password(password)
+        u.save()
+    
+        return render(request, "login.html", {'meg': 'Successfully done!'})
+
+    
+
+def user_login(request):
+        if request.method == 'POST':
+            data = request.POST
+            name = data.get('name')
+            password = data.get("pass")
+            u = authenticate(username=name, password=password)
+
+            if u is not None:
+                login(request,u)
+                return redirect("index")  
+            else:
+                return render(request, "login.html", {'err': 'Invalid credentials!'})
+
+    
+
+ 
+
+
+       
