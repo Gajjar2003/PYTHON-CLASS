bd8b558ec583dc66e65bcd415bd1d3bea8ffcf36
JENIL1108 <jenilgajjar1108@gmail.com>
2025-12-19 16:54:44 +0530

    all

---
+++ Class_work/14-Django/Django-Exam1/myapp/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render,redirect
+from myapp.models import *
+
+# Create your views here.
+def index(requset):
+    return render(requset,"index.html")
+
+def register(requset):
+    id = requset.POST.get('id')
+    name = requset.POST.get('name')
+    email = requset.POST.get('email')
+    age = requset.POST.get('age')
+    phone = requset.POST.get('phone')
+    image = requset.FILES.get('image')
+
+    if not id:
+        student.objects.create(name=name,email=email,age=age,phone=phone,image=image)
+
+        return render(requset,"index.html",{'meg':'successfully done !'})
+    else:
+       s = student.objects.get(pk=id)
+       s.name = name
+       s.email = email
+       s.age = age
+       s.phone = phone
+       if  image :
+           s.image = image
+       s.save()
+
+       return render(requset,"index.html",{'meg':'successfully update done !'})
+
+def display(requset):
+
+    students = student.objects.all()
+
+    return render(requset,"display.html",{'students': students})
+
+def delete(requset):
+
+    id = requset.GET.get('id')
+    s = student.objects.get(pk=id)
+    s.delete()
+    return redirect("display")
+
+
+
+
+
+def edit(requset):
+    id = requset.GET.get('id')
+    s = student.objects.get(pk=id)
+    return render(requset,"index.html",{'s':s})
