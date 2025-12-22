15d31e78abb35d5948b07d24fe1ff8236d46677a
JENIL1108 <jenilgajjar1108@gmail.com>
2025-12-22 19:47:44 +0530

    all

---
+++ Class_work/14-Django/Django-Exam3/myapp/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+
+
+# Create your views here.
+def index(requset):
+    if requset.method=='POST':
+        username = requset.POST['username']
+        password = requset.POST['password']
+        u = authenticate(username=username,password=password)
+
+        if u is None:
+             return render(requset,"index.html",{'err':'invalid username '})
+        else:
+            login(requset,u)
+            return redirect("home")
+            
+        
+        
+
+    
+    return render(requset,"index.html")
+
+def register(requset):
+    if requset.method=='POST':
+        fname = requset.POST['fname']
+        lname = requset.POST['lname']
+        username = requset.POST['username']
+        password = requset.POST['password']
+
+        if User.objects.filter(username=username).exists():
+             return render(requset,"register.html",{'err':'Username alredy exist !'})
+
+
+        u = User.objects.create(first_name = fname ,last_name=lname,username=username)
+        u.set_password(password)
+        u.save()
+
+        return render(requset,"register.html",{'meg':'successfully done !'})
+    
+    return render(requset,"register.html")
+@login_required(login_url="index")
+def home(requset):
+    return render(requset,"home.html")
+
+def user_logout(requset):
+  logout(requset)
+  return render(requset,"index.html")
