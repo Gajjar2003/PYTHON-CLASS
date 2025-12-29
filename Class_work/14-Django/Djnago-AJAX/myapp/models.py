d7d7f15f95995ca4ccd238bc74f7fe52d71565cd
JENIL1108 <jenilgajjar1108@gmail.com>
2025-12-29 11:27:02 +0530

    all

--- Class_work/14-Django/Djnago-AJAX/myapp/models.py
+++ Class_work/14-Django/Djnago-AJAX/myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class product(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class State(models.Model):
+    country = models.ForeignKey(Country,on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+
+
+class City(models.Model):
+    state = models.ForeignKey(State,on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
--- Class_work/14-Django/Djnago-AJAX/myapp/views.py
+++ Class_work/14-Django/Djnago-AJAX/myapp/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from myapp.models import *
+from django.http import JsonResponse
 
 # Create your views here.
 def index(requset):
     return render(requset,"index.html")
+
+# def view(requset):
+#     data = requset.GET['data']
+#     row = ""
+#     if data == 'sporst':
+#         row+= "<ul><li>Ball</li><li>Bat</li><li>Stmup</li></ul>"
+#     elif data == "electric":
+#         row+= "<ul><li>Tv</li><li>Pc</li><li>Laptop</li></ul>"
+#     else:
+#         row+="Not data found"
+
+#     return HttpResponse(row)
+
+def view(request):
+    data = request.GET['data']
+    products = product.objects.filter(name__startswith=data)
+    return JsonResponse({"products":list(products.values())})
+
+def countries(request):
+    countries = Country.objects.all()
+    return JsonResponse({"countries":list(countries.values())})
+
+def states(request):
+    cid = request.GET['cid']
+    states = State.objects.filter(country_id=cid)
+    return JsonResponse({"states":list(states.values())})
+
+def cities(request):
+    sid = request.GET['sid']
+    cities = City.objects.filter(state_id=sid)
+    return JsonResponse({"cities":list(cities.values())})
