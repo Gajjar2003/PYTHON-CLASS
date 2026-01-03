410350125826d0617557369e78459d42a03f466a
JENIL1108 <jenilgajjar1108@gmail.com>
2026-01-03 16:52:43 +0530

    all

--- Class_work/14-Django/Django-AJAX1/myapp/urls.py
+++ Class_work/14-Django/Django-AJAX1/myapp/urls.py
@@ -4,5 +4,11 @@ from myapp.views import *
 urlpatterns = [
     path("",index,name="index.html"),
     path("register",register,name="register"),
-    path("display",display,name="display")
+    path("display",display,name="display"),
+    path("delete",delete,name="delete"),
+    path("getbyid",getbyid,name="getbyid"),
+    path("update",update,name="update"),
+    path("search",search,name="search"),
+   
+   
 ]
--- Class_work/14-Django/Django-AJAX1/myapp/views.py
+++ Class_work/14-Django/Django-AJAX1/myapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from myapp.models import *
 from django.http import JsonResponse,HttpResponse
+from .models import student
+from django.db.models import Q
 
-# Create your views here.
-def index(requset):
-    return render(requset,"index.html")
+
+def index(request):
+    return render(request, "index.html")
+
 
 def register(request):
     if request.method == "POST":
@@ -12,10 +14,43 @@ def register(request):
         email = request.POST.get("email")
         phone = request.POST.get("phone")
 
-        student.objects.create(name=name,email=email,phone=phone)
+        student.objects.create( name=name,email=email,phone=phone)
 
-        return HttpResponse("Register successfully done !")
-    
-def display(requset):
+    return JsonResponse("Student Added Successfully", safe=False)
+
+
+
+def display(request):
     students = student.objects.all()
+    return JsonResponse({"students":list(students.values())})
+
+def delete(request):
+    sid = request.GET['sid']
+    st = student.objects.get(pk=sid)
+    st.delete()
+    return HttpResponse("Student Deleted")
+
+def getbyid(request):
+    sid = request.GET['sid']
+    st = student.objects.filter(id=sid)
+    return JsonResponse({"student":list(st.values())})
+
+def update(requset):
+     if requset.method == "POST":
+        id = requset.POST.get('id')
+        name = requset.POST.get("name")
+        email = requset.POST.get("email")
+        phone = requset.POST.get("phone")
+
+        s = student.objects.get(pk=id)
+        s.name =name
+        s.email =email
+        s.phone =phone
+        s.save()
+     
+        return JsonResponse("Update Successfully", safe=False)
+     
+def search(request):
+    value = request.GET['value']
+    students = student.objects.filter(Q(name__startswith=value)|Q(email__startswith=value)|Q(phone__startswith=value)) 
     return JsonResponse({"students":list(students.values())})
