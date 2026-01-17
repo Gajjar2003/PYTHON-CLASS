4ba5e94d3480d2f99024e6b347628f4edf201eb1
JENIL1108 <jenilgajjar1108@gmail.com>
2026-01-17 17:11:36 +0530

    all

--- Class_work/14-Django/Payment-Dajngo/myapp/views.py
+++ Class_work/14-Django/Payment-Dajngo/myapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 import razorpay
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your views here.
+
 def index(requset):
     return render(requset,"index.html")
 
@@ -13,3 +15,22 @@ def payment(requset):
     payment = client.order.create(data=data)
  
     return JsonResponse(payment)
+
+def sendmail(requset):
+    context = {}
+    if requset.method == 'POST':
+        address = requset.POST.get('address')
+        subject = requset.POST.get('subject')
+        message = requset.POST.get('message')
+
+        if address and subject and message:
+            try:
+                send_mail(subject, message, settings.EMAIL_HOST_USER, [address])
+                context['result'] = 'Email sent successfully'
+            except Exception as e:
+                context['result'] = f'Error sending email: {e}'
+        else:
+            context['result'] = 'All fields are required'
+    
+    return render(requset,"mail.html",context)
+   
