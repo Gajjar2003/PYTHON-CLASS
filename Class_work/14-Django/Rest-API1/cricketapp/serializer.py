d8f15078ae06cb0a8a9b4de6e6d46c57ad8d02ce
JENIL1108 <jenilgajjar1108@gmail.com>
2026-02-02 11:19:50 +0530

    all

--- Class_work/14-Django/Rest-API1/cricketapp/serializer.py
+++ Class_work/14-Django/Rest-API1/cricketapp/serializer.py
@@ -12,10 +12,11 @@ class cricketdeptserializer(serializers.ModelSerializer):
     class Meta:
         model = CricketDept
         fields = "__all__"
-        def to_representation(self, instance):
-            ret =  super().to_representation(instance)
-            ret['coach'] = coachserializer(instance.coach).data
-            return ret
+        
+    def to_representation(self, instance):
+        ret =  super().to_representation(instance)
+        ret['coach'] = coachserializer(instance.coach).data
+        return ret
 
 class cricketserializer(serializers.ModelSerializer):
     class Meta:
