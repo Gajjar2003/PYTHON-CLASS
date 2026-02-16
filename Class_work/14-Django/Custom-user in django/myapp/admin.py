444eb48e2c3b17b7ef0a47427a3a6659057490a5
JENIL1108 <jenilgajjar1108@gmail.com>
2026-02-16 11:32:53 +0530

    all

--- Class_work/14-Django/Custom-user in django/myapp/admin.py
+++ Class_work/14-Django/Custom-user in django/myapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from myapp.models import *
 
+admin.site.register(CustomeUser)
 # Register your models here.
--- Class_work/14-Django/Custom-user in django/myapp/models.py
+++ Class_work/14-Django/Custom-user in django/myapp/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from myapp.manager import *
+from django.contrib.auth.models import AbstractUser
 
 
-class CustomeUser():
+
+class CustomeUser(AbstractUser):
+    username = None
+    phone = models.CharField(max_length=20,unique=True)
+    bio = models.TextField()
+
+    USERNAME_FIELD = 'phone'
+    objects = CustomUserManager()
 
